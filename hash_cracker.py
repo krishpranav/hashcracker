539bcd74e5448a0f059ca9ba551f03f4d6ffabe5
krishpranav <krishna.pranav@gmail.com>
2020-08-03 00:45:47 -0400

    first commit

---
+++ hash_cracker.py
@@ -0,0 +1,31 @@
+import hashlib
+
+flag = 0
+counter = 0
+
+pass_hash = input("Enter Your md5 hash >> ")
+word_list = input("Enter Your Path To Password List >> ")
+
+try:
+	pass_file = open(wordlist, "r")
+except:
+	print("No File Found :( :(")
+	print("Exiting....")
+	quit()
+
+for word in pass_file:
+
+	enc_wrd = word.encode('utf-8')
+	digest = hashlib.md5(enc_wrd.strip()).hexdigest()
+
+	print(word)
+	print(digest)
+	print(pass_hash)
+	if digest == pass_hash:
+		print("Password Found")
+		print("Password Is " + word)
+		flag = 1
+		break
+
+if flag == 0:
+	print("Password/Passphrase is not in the list :( :(")
